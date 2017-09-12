4846faca5ef7d86a6ecf4db54cfa1106d8c11fd5
Kyle Isom <kyle@imap.cc>
2017-09-12 00:42:45 -0700

    Add more packages; refresh contents as neeed.

--- .emacs.d/ensure.el
+++ .emacs.d/ensure.el
@@ -2,10 +2,17 @@
   (unless (package-installed-p package)
     (package-install package)))
 
+(unless (file-directory-p "/home/kyle/.emacs.d/elpa/archives/melpa")
+  (package-refresh-contents))
+
 (let ((initial-package-list
        '(anaconda-mode
 	 auto-complete
 	 cargo
+	 go-autocomplete
+	 go-direx
+	 go-guru
+	 go-mode
 	 irfc
 	 magit
 	 markdown-mode

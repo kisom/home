e267132df1e86b0b9fceb74a266502fc49deab86
Kyle Isom <kyle@imap.cc>
2019-04-15 21:46:25 +0000

    Enable projectile mode on startup.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -109,6 +109,8 @@
 (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 (setq projectile-project-search-path '("~/src/" "~/code/"))
+(projectile-mode +1)
+
 
 ;;; 
 ;;;                                                      _:_
